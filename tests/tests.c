e1f51f1c5ed7544b6c5c40f7ad70d605934d156b
LucasTavaresA <tavares.lassuncao@gmail.com>
2025-10-28 18:45:15 -0300

    Add snapshot tests

---
+++ tests/tests.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+FILE* out;
+#define LOG_OUT out
+#include "../log.c"
+
+#define MAKEARGS_DRY_RUN
+#define MAKEARGS_TARGETS \
+	MAKEARGS_TARGET(build) \
+	MAKEARGS_TARGET(run)
+
+#define MAKEARGS_IMPLEMENTATION
+#include "../makeargs.c"
+
+#define SEPARATOR \
+	"--------------------------------------------------------------------------------\n"
+#define ARGS(...)                                               \
+	{                                                             \
+		sizeof((const char*[]){__VA_ARGS__}) / sizeof(const char*), \
+				(const char*[])                                         \
+		{                                                           \
+			__VA_ARGS__                                               \
+		}                                                           \
+	}
+
+typedef struct
+{
+	int argc;
+	const char** argv;
+} args;
+
+const args test_args[] = {
+		ARGS("./test"),
+		ARGS("./test", "build"),
+		ARGS("./test", "build", "run"),
+		ARGS("./test", "--"),
+		ARGS("./test", "build", "--"),
+		ARGS("./test", "run", "build", "--"),
+		ARGS("./test", "--", "-s"),
+		ARGS("./test", "build", "--", "-s"),
+		ARGS("./test", "build", "run", "--", "-s"),
+		ARGS("./test", "--", "-s", "--test"),
+		ARGS("./test", "build", "--", "-s"),
+		ARGS("./test", "run", "build", "--", "-s", "--test"),
+		ARGS("./test", "CONTAINER=docker"),
+		ARGS("./test", "run", "CONTAINER=docker", "build"),
+		ARGS("./test", "CONTAINER="),
+		ARGS("./test", "run", "CONTAINER=", "build"),
+		ARGS("./test", "build", "VAR="),
+		ARGS("./test", "VAR=", "build"),
+		ARGS("./test", "build", "--", "DEBUG=1"),
+		ARGS("./test", "build", "--", "DEBUG="),
+		ARGS("./test", "FOO=bar", "--", "-s"),
+		ARGS("./test", "--", "FOO=bar"),
+		ARGS("./test", "FOO=", "--", "-s"),
+		ARGS("./test", "--", "FOO="),
+};
+
+void print_args(const int argc, const char** argv)
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		LOG_PRINTF("%s ", argv[i]);
+		i++;
+	}
+	LOG_PRINTF("\n");
+}
+
+int main(int argc, const char** argv)
+{
+	if (argc > 1 && strcmp(argv[1], "save") == 0)
+	{
+		out = fopen("expected", "w");
+	}
+	else
+	{
+		out = fopen("output", "w");
+	}
+
+	{
+		makeargs_vars_count = 0;
+
+		for (int i = 0; i < sizeof(test_args) / sizeof(test_args[0]); i++)
+		{
+			print_args(test_args[i].argc, test_args[i].argv);
+			int ret = makeargs_run_targets(test_args[i].argc, test_args[i].argv);
+			LOG_PRINTF("%d <- makeargs_run_targets()\n", ret);
+
+			ret = makeargs_set_vars(test_args[i].argc, test_args[i].argv);
+			LOG_PRINTF("%d <- makeargs_set_vars()\n", ret);
+
+			for (int i = 0; i < makeargs_vars_count; i++)
+			{
+				LOG_PRINTF("%s=\"%s\"\n", makeargs_vars[i].name,
+									 makeargs_vars[i].value);
+			}
+
+			makeargs_vars_count = 0;
+			LOG_PRINTF(SEPARATOR);
+		}
+	}
+
+	fclose(out);
+
+	system("diff output expected");
+	return 0;
+}
